1f58efd4af50f285f9996128ef421f6dc1b99706
SamuelHFS <samuel.jpg1@gmail.com>
2021-10-06 09:26:44 -0300

    nada pronto

---
+++ ProjetoMaster/ProjetoMaster/ProjetoMaster/Model/Times.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoMaster.Model
+{
+    class Times
+    {
+        public string nome { get; set; }
+
+        public string imagem { get; set; }
+
+        public string descricao { get; set; }
+
+        public string tecnico { get; set; }
+
+        public string[] jogadores { get; set; }
+    }
+}
